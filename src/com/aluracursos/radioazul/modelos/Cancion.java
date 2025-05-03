8c2a5f60e7249bdd20aa9cbf053a03eacad9e86f
Santiago Bertorello <santinbertorello@gmail.com>
2025-05-03 19:29:27 -0300

    add: modelo Cancion

---
+++ src/com/aluracursos/radioazul/modelos/Cancion.java
@@ -0,0 +1,34 @@
+package com.aluracursos.radioazul.modelos;
+
+public class Cancion {
+
+    private String album;
+
+    private String cantante;
+
+    private String genero;
+
+    public String getAlbum() {
+        return album;
+    }
+
+    public void setAlbum(String album) {
+        this.album = album;
+    }
+
+    public String getCantante() {
+        return cantante;
+    }
+
+    public void setCantante(String cantante) {
+        this.cantante = cantante;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+}
